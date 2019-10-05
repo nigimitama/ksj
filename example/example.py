10c330e699a701fdfe87f7a4cdfd0d70e90ce4e3
nigimitama <nigimitama16@gmail.com>
2019-10-05 20:18:41 +0900

    国土数値情報APIを使ったシェープファイルのダウンロード機能を実装

---
+++ example/example.py
@@ -0,0 +1,19 @@
+from src import ksj
+
+# 国土数値情報APIでダウンロードできるファイル概要（pd.DataFrame）
+ksj.get_summary()
+
+# 国土数値情報のzipファイルのURLを取得（pd.DataFrame）
+identifier = "N03"
+urls = ksj.get_url(identifier=identifier, pref_code='11-14', fiscal_year=2019)
+urls.head()
+
+# zipファイルのダウンロードと解凍
+url = urls["zipFileUrl"][0]
+ksj.download_and_unzip(url, save_path="./example/shapefile/")
+
+
+# 列名の対応表をダウンロードして結合して開く（pd.DataFrame）
+book = ksj.get_column_table()
+book.query(f"識別子 == '{identifier}'")
+
---
+++ src/ksj.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import requests
+from lxml import etree
+import xmljson
+
+
+def get_summary() -> pd.DataFrame:
+    """
+    国土数値情報の概要情報（取得できるデータ一覧）を取得する
+    """
+    # const params
+    app_id="ksjapibeta1"
+    lang="J"
+    data_format=1
+    # set url and params
+    api_url = "http://nlftp.mlit.go.jp/ksj/api/1.0b/index.php/app/getKSJSummary.xml"
+    # 仕様書では'dataFormat'なのだが、実際は'dataformat'に送らないとエラーになる
+    params = {"appId": app_id, "lang": lang, "dataformat": data_format}
+    # request
+    response = requests.get(api_url, params=params)
+    root = etree.fromstring(response.content)
+    # convert xml to dict
+    data_dict = xmljson.yahoo.data(root)
+    # error
+    error_msg = data_dict["KSJ_SUMMARY_INF"]["RESULT"]["ERROR_MSG"]
+    if error_msg != "正常に終了しました。":
+        print(error_msg)
+    else:
+        # convert dict to pd.DataFrame
+        data_df = pd.io.json.json_normalize(data_dict["KSJ_SUMMARY_INF"]["KSJ_SUMMARY"]["item"])
+    return data_df
+
+
+def get_url(identifier, pref_code=None, mesh_code=None, metro_area=None, fiscal_year=None):
+    """
+    国土数値情報のURLを取得する
+    """
+    # const params
+    app_id="ksjapibeta1"
+    lang="J"
+    data_format=1
+    # set url and params
+    api_url = "http://nlftp.mlit.go.jp/ksj/api/1.0b/index.php/app/getKSJURL.xml"
+    params = {"appId": app_id, "lang": lang, "dataformat": data_format, "identifier": identifier}
+    if pref_code:
+        params["prefCode"] = pref_code
+    if mesh_code:
+        params["meshCode"] = mesh_code
+    if metro_area:
+        params["metroArea"] = metro_area
+    if fiscal_year:
+        params["fiscalyear"] = fiscal_year
+    # request
+    response = requests.get(api_url, params=params)
+    # convert to xml
+    root = etree.fromstring(response.content)
+    # convert xml to dict
+    data_dict = xmljson.yahoo.data(root)
+
+    error_msg = data_dict["KSJ_URL_INF"]["RESULT"]["ERROR_MSG"]
+    if error_msg != "正常に終了しました。":
+        print(error_msg)
+    else:
+        # convert dict to pd.DataFrame
+        data_df = pd.io.json.json_normalize(data_dict["KSJ_URL_INF"]["KSJ_URL"]["item"])
+    return data_df
+
+
+
+from urllib.request import urlretrieve
+import zipfile
+
+def download_and_unzip(file_url, save_path, save_file_name=None):
+    """
+    指定したURLのzipファイルをダウンロードし、解凍する
+    """
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+    save_file_name = os.path.basename(file_url) if save_file_name is None else save_file_name
+    save_file_name = os.path.join(save_path, save_file_name)
+    urlretrieve(url=file_url, filename=save_file_name)
+    # extract to save_dir
+    with zipfile.ZipFile(save_file_name) as existing_zip:
+        existing_zip.extractall(save_path)
+       
+
+import xlrd
+from urllib.request import urlretrieve
+import tempfile
+import os
+
+
+def get_shape_property_table(silent=True) -> xlrd.book.Book:
+    """
+    列名の対応表xlsファイルを一時フォルダにダウンロードし、開く。
+    xlsファイルは開いてすぐ（メソッドの処理が終わるとともに）消去される
+    """
+    url = "http://nlftp.mlit.go.jp/ksj/gml/shape_property_table.xls"
+    file_name = os.path.basename(url)
+    temp_dir = tempfile.TemporaryDirectory()
+    file_path = os.path.join(temp_dir.name, file_name)
+    urlretrieve(url=url, filename=file_path)
+    if not silent:
+        print(f"shape_property_table is downloaded at {file_path}")
+    return xlrd.open_workbook(file_path)
+
+
+def get_column_table(silent=True) -> pd.DataFrame:
+    """
+    列名の対応表xlsファイルを開き、全シートを結合して単一のデータフレームにして返す。
+    """
+    book = get_shape_property_table(silent=silent)
+    sheets = []
+    for sheet_name in book.sheet_names():
+        sheet = get_sheet(book=book, sheet_name=sheet_name)
+        sheets.append(sheet)
+    return pd.concat(sheets, axis=0, sort=False)
+
+
+def get_sheet(book: xlrd.book.Book, sheet_name=False) -> pd.DataFrame:
+    """
+    指定したsheet nameのシートをpandasで開く
+    """
+    sheet = book.sheet_by_name(sheet_name)
+    table = [sheet.row_values(row) for row in range(sheet.nrows)]
+    index = get_content_start_row_index(table)
+    return pd.DataFrame(table[index+1:], columns=table[index])
+
+
+def get_content_start_row_index(table: list) -> int:
+    """
+    表頭っぽい行にきたらbreakして行番号を取得する
+    """
+    for i in range(len(table)):
+        if ("属性名" in table[i]) or ("データ項目" in table[i]):
+            break
+    return i
