13df8a72a24337b1e18ea75e3aa50ff4a84fe15a
nigimitama <nigimitama16@gmail.com>
2019-10-15 08:13:56 +0900

    列名の変換機能を実装（暫定版）

--- example/example.py
+++ example/example.py
@@ -1,18 +1,25 @@
-import kokudo_suuchi as ksj
+import kokudo_suuchi_jouhou as ksj
 
-# 国土数値情報APIでダウンロードできるファイル概要（pd.DataFrame）
+# 国土数値情報APIでダウンロードできるファイル概要（return: pd.DataFrame）
 ksj.get_summary()
 
-# 国土数値情報のzipファイルのURLを取得（pd.DataFrame）
+# 国土数値情報のzipファイルのURLを取得（return: pd.DataFrame）
 identifier = "N03"
 urls = ksj.get_url(identifier=identifier, pref_code='11-14', fiscal_year=2019)
 urls.head()
 
+# ファイルを指定フォルダあるいは一時フォルダにダウンロードし、解凍してgeopandasで読み込む
+url = urls["zipFileUrl"][0]
+shape_gdf = ksj.download_and_load(url, silent=False)
+shape_gdf
+
+# 簡単に自動変換できる列名（年度によって意味が変わらない列）に限り、列名を日本語に変換
+ksj.translate(shape_gdf)
+
 # zipファイルのダウンロードと解凍
 url = urls["zipFileUrl"][0]
-ksj.download_and_unzip(url, save_path="./example/shapefile/")
+ksj.download_and_unzip(url, save_dir="./example/shapefile/", silent=False)
 
-# 列名の対応表をダウンロードして結合して開く（pd.DataFrame）
+# 列名の対応表をダウンロードして結合して開く（return: pd.DataFrame）
 book = ksj.get_column_table()
 book.query(f"識別子 == '{identifier}'")
-
--- src/kokudo_suuchi.py
+++ src/kokudo_suuchi.py
@@ -1,17 +1,23 @@
+import os
+import tempfile
+import xlrd
+import zipfile
+from urllib.request import urlretrieve
 import pandas as pd
 import requests
 from lxml import etree
 import xmljson
-
+import geopandas as gpd
+import json
 
 def get_summary() -> pd.DataFrame:
     """
     国土数値情報の概要情報（取得できるデータ一覧）を取得する
     """
     # const params
-    app_id="ksjapibeta1"
-    lang="J"
-    data_format=1
+    app_id = "ksjapibeta1"
+    lang = "J"
+    data_format = 1
     # set url and params
     api_url = "http://nlftp.mlit.go.jp/ksj/api/1.0b/index.php/app/getKSJSummary.xml"
     # 仕様書では'dataFormat'なのだが、実際は'dataformat'に送らないとエラーになる
@@ -27,7 +33,8 @@ def get_summary() -> pd.DataFrame:
         print(error_msg)
     else:
         # convert dict to pd.DataFrame
-        data_df = pd.io.json.json_normalize(data_dict["KSJ_SUMMARY_INF"]["KSJ_SUMMARY"]["item"])
+        data_df = pd.io.json.json_normalize(
+            data_dict["KSJ_SUMMARY_INF"]["KSJ_SUMMARY"]["item"])
     return data_df
 
 
@@ -36,12 +43,13 @@ def get_url(identifier, pref_code=None, mesh_code=None, metro_area=None, fiscal_
     国土数値情報のURLを取得する
     """
     # const params
-    app_id="ksjapibeta1"
-    lang="J"
-    data_format=1
+    app_id = "ksjapibeta1"
+    lang = "J"
+    data_format = 1
     # set url and params
     api_url = "http://nlftp.mlit.go.jp/ksj/api/1.0b/index.php/app/getKSJURL.xml"
-    params = {"appId": app_id, "lang": lang, "dataformat": data_format, "identifier": identifier}
+    params = {"appId": app_id, "lang": lang,
+              "dataformat": data_format, "identifier": identifier}
     if pref_code:
         params["prefCode"] = pref_code
     if mesh_code:
@@ -62,32 +70,66 @@ def get_url(identifier, pref_code=None, mesh_code=None, metro_area=None, fiscal_
         print(error_msg)
     else:
         # convert dict to pd.DataFrame
-        data_df = pd.io.json.json_normalize(data_dict["KSJ_URL_INF"]["KSJ_URL"]["item"])
+        data_df = pd.io.json.json_normalize(
+            data_dict["KSJ_URL_INF"]["KSJ_URL"]["item"])
     return data_df
 
 
-
-from urllib.request import urlretrieve
-import zipfile
-
-def download_and_unzip(file_url, save_path, save_file_name=None):
+def download_and_unzip(file_url, save_dir, save_file_name=None, silent=None):
     """
-    指定したURLのzipファイルをダウンロードし、解凍する
+    指定したURLのzipファイルを指定フォルダにダウンロードし、解凍する
     """
-    if not os.path.exists(save_path):
-        os.mkdir(save_path)
-    save_file_name = os.path.basename(file_url) if save_file_name is None else save_file_name
-    save_file_name = os.path.join(save_path, save_file_name)
-    urlretrieve(url=file_url, filename=save_file_name)
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    save_file_name = os.path.basename(
+        file_url) if save_file_name is None else save_file_name
+    save_path = os.path.join(save_dir, save_file_name)
+    urlretrieve(url=file_url, filename=save_path)
     # extract to save_dir
-    with zipfile.ZipFile(save_file_name) as existing_zip:
-        existing_zip.extractall(save_path)
-       
+    with zipfile.ZipFile(save_path) as existing_zip:
+        existing_zip.extractall(save_dir)
+    if not silent:
+        print(f"{save_file_name} is extracted to {save_dir}")
 
-import xlrd
-from urllib.request import urlretrieve
-import tempfile
-import os
+
+def download_and_load(file_url, save_dir=None, save_file_name=None, silent=None):
+    """
+    指定したURLのzipファイルを指定フォルダあるいは一時フォルダにダウンロードし、
+    解凍してgeopandasで開く
+    """
+    if save_file_name is None:
+        save_file_name = os.path.basename(file_url)
+    if save_dir is None:
+        temp_dir = tempfile.TemporaryDirectory()
+        save_dir = temp_dir.name
+    elif not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    save_path = os.path.join(save_dir, save_file_name)
+    urlretrieve(url=file_url, filename=save_path)
+    if not silent:
+        print(f"{save_file_name} is extracted to {save_dir}")
+    # extract to save_dir
+    with zipfile.ZipFile(save_path) as existing_zip:
+        existing_zip.extractall(save_dir)
+    # load
+    file_names = get_files(save_dir)
+    shapefiles = [file_name for file_name in file_names if ".shp" in file_name]
+    modify_times = [os.lstat(shapefile).st_mtime for shapefile in shapefiles]
+    sorted_indices = sorted(range(len(modify_times)),
+                            key=lambda k: modify_times[k], reverse=True)
+    newest_mod_file = shapefiles[sorted_indices[0]]
+    file_path = os.path.join(save_dir, newest_mod_file)
+    print(f"Loading {file_path}")
+    return gpd.read_file(file_path)
+
+
+def get_files(path: str) -> list:
+    """get file pathes recursively"""
+    file_names = []
+    for (root, dirs, files) in os.walk(path):
+        for f in files:
+            file_names.append(os.path.join(root, f))
+    return file_names
 
 
 def get_shape_property_table(silent=True) -> xlrd.book.Book:
@@ -135,3 +177,10 @@ def get_content_start_row_index(table: list) -> int:
         if ("属性名" in table[i]) or ("データ項目" in table[i]):
             break
     return i
+
+
+def translate(data: pd.DataFrame) -> pd.DataFrame:
+    """自動変換できる列（年度によって意味が変わらない列）に限り、列名を変換する"""
+    with open("./data/rename_dict.json", "r") as f:
+        rename_dict = json.load(f)
+    return data.rename(columns=rename_dict)
